d0d085732e32abdc4ed90e1753a87440c93f65a3
nadeem-github <m.nadeempatel@gmail.com>
2025-07-01 19:37:00 +0530

    pushed electron application

---
+++ main.js
@@ -0,0 +1,29 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // Load the Angular build output
+    win.loadFile(path.join(__dirname, 'dist/next-frontend-test/index.html'));
+
+    // Uncomment to open devtools
+    // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
